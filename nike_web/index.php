e866ed44494853341c44e5277cde7a4104540765
ellodikia <gabriellaodikia@gmail.com>
2025-09-14 12:18:22 +0700

    update 1

---
+++ nike_web/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  </head>
+  <body>
+    <div class="bg-blue"> 
+        <h1 class="text-4xl">Nike</h1>
+        <div class=""></div>
+    </div>
+  </body>
+</html>
